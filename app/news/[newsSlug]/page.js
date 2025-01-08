af78e49c58223dbe75bfdf29bdfae506e33b00a2
nishaaa25 <sahnisha2505@gmail.com>
2025-01-08 21:31:42 +0530

    added-dummy-news-data

--- app/news/[newsSlug]/page.js
+++ app/news/[newsSlug]/page.js
@@ -1,7 +1,12 @@
+// import Image from "next/image";
+// import Link from "next/link";
+
 export default function NewsItem() {
   return (
-    <div>
-      <p>hey</p>
+    <div className="w-screen min-h-screen relative">
+    <div className="w-9/12 mx-auto py-20">
+    hey
     </div>
+  </div>
   );
 }
