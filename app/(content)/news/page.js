1edbc72a6ca2c48dbe92f3bee843d6aa3578651c
nishaaa25 <sahnisha2505@gmail.com>
2025-01-13 10:19:32 +0530

    fetching-data-directly-from-database&&moved-data.db-to-root-folder

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,44 +1,64 @@
-"use client";
+// "use client";
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 // import { getAllNews } from "@/lib/news";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [news, setNews] = useState([]);
-  const [error, setError] = useState("");
+export default async function NewsPage() {
+  // FETCHING DATA DIRECTLY FROM DATABASE WITHOUT USING SEPERATE BACKEND
+  // this is the best way to fetch data in nextjs application if you own the database
+  const news = getAllNews();
 
-  useEffect(() => {
-    async function getNews() {
-      const response = await fetch("http://localhost:8080/news");
-      const news = await response?.json();
-      setNews(news);
+  // USING FETCH IN SERVER COMPONENTS TO FETCH DATA FROM SEPERATE BACKEND
+  // you can directly fetch data in nextjs like this as nextjs offers server components that runs on server only. In nextjs, you can still use fetch method it is allowed by nodejs and nextjs extends this fetch function and add some extra caching related fucntions to it.
 
-      if (!response.ok) {
-        setError("Failed to fetch news");
-      }
-    }
+  //  you can use next server components for fetching data by turning it into an async function. and you can use fetch method inside it without using useeffect hooks.
+  // const response = await fetch ("http://localhost:8080/news");
+  // const news = response.json();
 
-    getNews();
-  }, []);
+  // let newsContent;
 
-  let newsContent;
+  // this code is not required anymore in this function coz this will always wait till the await resolves as this is an async function. 
+  // if (news) {
+  //   newsContent = <NewsList news={news} />;
+  // }
 
-  if (!news) {
-    newsContent = <p>{error}</p>;
-    console.log(error);
-  }
+  // USING CLIENT COMPONENTS TO FETCH DATA FROM SEPERATE BACKEND
+  // you can fetch data in nextjs like this but this is not a good way to fetch data in nextjs. it turns out our react server component into client component which is not good.
+  // const [news, setNews] = useState([]);
+  // const [error, setError] = useState("");
 
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  // useEffect(() => {
+  //   async function getNews() {
+  //     const response = await fetch("http://localhost:8080/news");
+  //     const news = await response?.json();
+  //     setNews(news);
+
+  //     if (!response.ok) {
+  //       setError("Failed to fetch news");
+  //     }
+  //   }
+
+  //   getNews();
+  // }, []);
+
+  // let newsContent;
+
+  // if (!news) {
+  //   newsContent = <p>{error}</p>;
+  //   console.log(error);
+  // }
+
+  // if (news) {
+  //   newsContent = <NewsList news={news} />;
+  // }
 
   return (
     <div className="w-screen min-h-screen relative">
       <div className="w-9/12 mx-auto py-20">
         <p className="text-3xl font-semibold mb-6">News Page</p>
-        {newsContent}
+        {/* {newsContent} */}
+        <NewsList news={news}/>
       </div>
     </div>
   );
